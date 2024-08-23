db687524d379f86ed4e7a2177e32db0b50e6fd22
Daniel17971 <109083455+Daniel17971@users.noreply.github.com>
2024-08-23 09:52:21 +0100

    tracks

---
+++ spotify_request_tracks.py
@@ -0,0 +1,20 @@
+import requests
+import os
+from auth_code import get_auth_header
+import json
+
+
+def return_artists(arr):
+                return arr['name']
+
+def get_tracks_and_artists(token,limit):
+        my_playlist= requests.get(f"https://api.spotify.com/v1/playlists/3MCN79RFWj2MKwcPC3K3ZB/tracks?market=GB&limit={limit}",
+                                headers=get_auth_header(token))
+        playlist_dict=json.loads(my_playlist.content)
+        trackArr=[]
+
+        for track in playlist_dict['items']:
+                trackArr.append({"name":track['track']['name'],"artists":list(map(return_artists,track['track']['artists']))})
+        
+        return trackArr
+        
