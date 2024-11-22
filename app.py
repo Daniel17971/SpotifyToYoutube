c54836196eac56685269d8da6589c681b44c65a8
Daniel17971 <109083455+Daniel17971@users.noreply.github.com>
2024-11-22 14:29:52 +0000

    style: added some comments

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ API_VERSION = 'v2'
 FLASK_API_KEY= os.getenv("FLASK_API_KEY")
 app = Flask(__name__)
 # Note: A secret key is included in the sample so that it works.
-# If you use this code in your application, replace this with a truly secret
-# key. See https://flask.palletsprojects.com/quickstart/#sessions.
 app.secret_key = FLASK_API_KEY
 AUTH_URL='https://accounts.google.com/o/oauth2/auth'
 
@@ -54,10 +52,9 @@ def logout():
 
 if __name__ == '__main__':
   # When running locally, disable OAuthlib's HTTPs verification.
-  # ACTION ITEM for developers:
-  #     When running in production *do not* leave this option enabled.
+  # When running in production *do not* leave this option enabled.
   os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
   # Specify a hostname and port that are set as a valid redirect URI
-  # for your API project in the Google API Console.
+  # for your API project in the services API consoles, spotify and google
   app.run('localhost', 8080, debug=True)
